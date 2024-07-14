b8616b4f0b5a6530071b8cf4dc34d7fbafba5184
AhasanulMusanna <ahasanulmusanna@outlook.com>
2024-07-15 00:59:21 +1000

    User Input, Warning Message, Print Input

---
+++ Assessment 1/Assessment1.java
@@ -0,0 +1,40 @@
+/**
+ * PROG5001 - Fundamentals of Programming
+ * Assessment 1
+ * Ahasanul Kabir Musanna
+ * Student ID: 24352976
+ */
+
+import java.util.Scanner ;
+
+public class Assessment1
+{
+    public static void main(String[] args) {
+        int NumberOfStudents = 30;
+        float[] marks = new float[NumberOfStudents];
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the Assignment Name: ");
+        String AssignmentName = scanner.nextLine();
+        
+        System.out.println("Input Marks of the Students: ");
+        
+        for(int i=0; i< NumberOfStudents; i++) {
+            float temp = scanner.nextFloat();
+            
+            while (temp < 0 || temp > 30) {
+                System.out.println("Invalid Mark! Please Enter the Mark Again");
+                temp = scanner.nextFloat();
+            }
+            
+            marks[i] = temp;
+        }
+        
+        System.out.println("Marks of the Students for " + AssignmentName + ":");
+        for(int j=0; j<NumberOfStudents; j++){
+            System.out.println(marks[j]);
+        }
+        
+        
+    }
+}
